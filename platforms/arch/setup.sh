bc7a6836d2ccc5a1aefe32cd43e9c8e7bc9b6402
Niels Nielsen <niels.nielsen@gmail.com>
2025-05-30 18:37:03 -0600

    arch setup is able to install latest version of nodejs and run setup.ts script

--- platforms/arch/setup.sh
+++ platforms/arch/setup.sh
@@ -1,3 +1,35 @@
 #!/bin/sh
 
-echo "running setup for arch"
+setup_dir=$(dirname "$(realpath "$0")")
+
+# base dev packages
+sudo pacman -Sy --needed --noconfirm git base-devel go rustup
+
+# only run this block if yay is NOT on your PATH
+if ! which yay >/dev/null 2>&1; then
+  echo "Installing yay AUR helper…"
+  pushd /tmp
+  if [ ! -d "yay" ]; then
+    git clone https://aur.archlinux.org/yay.git
+  fi
+  pushd yay
+  makepkg -si --noconfirm
+  popd
+  popd
+fi
+
+# install fnm and latest version of nodejs
+yay -S --needed --noconfirm fnm
+
+eval "$(fnm env --use-on-cd)"
+
+fnm install --latest
+fnm default latest
+
+# run setup script
+pushd ${setup_dir}
+
+npm install
+node setup.ts
+
+popd
