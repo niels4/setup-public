2ca94250691bfa1a5c8a128f5fae46bd6480a22e
Niels Nielsen <niels.nielsen@gmail.com>
2025-05-30 19:17:03 -0600

    mac: install latest version of nodejs and run setup.ts script

--- platforms/mac/setup.sh
+++ platforms/mac/setup.sh
@@ -1,3 +1,55 @@
 #!/bin/sh
 
-echo "running setup for mac"
+setup_dir=$(dirname "$(realpath "$0")")
+
+
+if [ ! -d /opt/homebrew ];then
+
+  echo "Enter user password to install homebrew:"
+  IFS= read -rs USER_PW
+  sudo -k # clear sudo credentials
+  if echo "$USER_PW" | sudo -S -v &> /dev/null; then
+    echo "\nPassword confirmed."
+  else
+    echo "\nInvalid password."
+    sudo -k
+    exit 1
+  fi
+  sudo -k # clear sudo credentials
+
+  homebrew_install_script="/tmp/homebrew_install.sh"
+  echo '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"' > ${homebrew_install_script}
+  chmod +x ${homebrew_install_script}
+
+  expect << EOF
+set timeout -1
+spawn ${homebrew_install_script}
+
+expect "Password:"
+send -- "$USER_PW\r"
+
+expect -re {to continue or any other key to abort:\r\n$}
+send -- "\r"
+
+expect eof
+EOF
+
+  USER_PW=""
+
+  echo "Homebrew installed, run brew doctor to verify"
+fi
+
+# install homebrew and latest version of nodes
+eval "$(/opt/homebrew/bin/brew shellenv)"
+brew install fnm
+eval "$(fnm env --use-on-cd)"
+fnm install --latest
+fnm default latest
+
+# run setup script
+pushd ${setup_dir}
+
+npm install
+node setup.ts
+
+popd
