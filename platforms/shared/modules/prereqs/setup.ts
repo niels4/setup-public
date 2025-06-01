c246cf3d8159d8a1acdcb7a6e5d6df96bbb76030
Niels Nielsen <niels.nielsen@gmail.com>
2025-06-01 11:11:50 -0600

    add more XDG vars from the start. add path for cargo installed binaries

--- platforms/shared/modules/prereqs/setup.ts
+++ platforms/shared/modules/prereqs/setup.ts
@@ -4,11 +4,12 @@ import { addLineToZshenv } from "#shared/util.ts"
 const configDir = `export XDG_CONFIG_HOME="${configHome}"`
 const dataDir = `export XDG_DATA_HOME="${dataHome}"`
 const cacheDir = 'export XDG_CACHE_HOME="${HOME}/.cache"'
+const stateDir = "export XDG_STATE_HOME=${HOME}/.local/state"
 const fnmEnv = 'eval "$(fnm env --use-on-cd)"'
 const goPath = 'export PATH="${HOME}"/go/bin:${PATH}'
 const nodePath = 'export PATH="./node_modules/.bin:${PATH}"'
-const cargoHome = 'export CARGO_HOME="$XDG_DATA_HOME/cargo"'
-const rustupHome = 'export RUSTUP_HOME="$XDG_CONFIG_HOME/rustup"'
+const cargoHome = 'export CARGO_HOME="${XDG_DATA_HOME}/cargo"'
+const rustupHome = 'export RUSTUP_HOME="${XDG_CONFIG_HOME}/rustup"'
 const rustPath = 'export PATH="${CARGO_HOME}/bin:${PATH}"'
 
 // Add required utilities and environment variables that all scripts can rely on
@@ -16,6 +17,7 @@ export default async function setup() {
   await addLineToZshenv(configDir)
   await addLineToZshenv(dataDir)
   await addLineToZshenv(cacheDir)
+  await addLineToZshenv(stateDir)
   await addLineToZshenv(fnmEnv)
   await addLineToZshenv(goPath)
   await addLineToZshenv(nodePath)
