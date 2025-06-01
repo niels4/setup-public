ce2051ebb99132a4dd6261cb3e24628b18f63d88
Niels Nielsen <niels.nielsen@gmail.com>
2025-06-01 17:40:39 -0600

    add homebrew vars to zshenv

--- platforms/mac/modules/prereqs/setup.ts
+++ platforms/mac/modules/prereqs/setup.ts
@@ -1,11 +1,17 @@
 import { brewBundle } from "#mac/mac-util.ts"
 import { zshenv } from "#shared/constants.ts"
 import sharedPrereqsSetup from "#shared/modules/prereqs/setup.ts"
+import { addLineToZshenv } from "#shared/util.ts"
 import fs from "fs-extra"
+
+const homebrewUpdateRate = `export HOMEBREW_AUTO_UPDATE_SECS=43200`
+const homebrewEnv = `eval "$(/opt/homebrew/bin/brew shellenv)"`
 
 // Add required utilities and environment variables that all scripts can rely on
 export default async function setup() {
   await fs.ensureFile(zshenv)
+  await addLineToZshenv(homebrewUpdateRate)
+  await addLineToZshenv(homebrewEnv)
   await brewBundle(import.meta.dirname)
   await sharedPrereqsSetup()
 }
