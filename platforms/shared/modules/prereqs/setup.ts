36a0fdf7f5debad1e98409139ea4d377a1164cbf
Niels Nielsen <niels.nielsen@gmail.com>
2025-06-01 10:53:49 -0600

    move rust folders out of home directory. Add cargo installed binaries to path

--- platforms/shared/modules/prereqs/setup.ts
+++ platforms/shared/modules/prereqs/setup.ts
@@ -7,6 +7,9 @@ const cacheDir = 'export XDG_CACHE_HOME="${HOME}/.cache"'
 const fnmEnv = 'eval "$(fnm env --use-on-cd)"'
 const goPath = 'export PATH="${HOME}"/go/bin:${PATH}'
 const nodePath = 'export PATH="./node_modules/.bin:${PATH}"'
+const cargoHome = 'export CARGO_HOME="$XDG_DATA_HOME/cargo"'
+const rustupHome = 'export RUSTUP_HOME="$XDG_CONFIG_HOME/rustup"'
+const rustPath = 'export PATH="${CARGO_HOME}/bin:${PATH}"'
 
 // Add required utilities and environment variables that all scripts can rely on
 export default async function setup() {
@@ -16,4 +19,7 @@ export default async function setup() {
   await addLineToZshenv(fnmEnv)
   await addLineToZshenv(goPath)
   await addLineToZshenv(nodePath)
+  await addLineToZshenv(cargoHome)
+  await addLineToZshenv(rustupHome)
+  await addLineToZshenv(rustPath)
 }
