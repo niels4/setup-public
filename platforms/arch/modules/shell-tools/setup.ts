c4bfa4384844027a5c3a2e306b541c1742bebc59
Niels Nielsen <niels.nielsen@gmail.com>
2025-06-02 01:29:52 +0000

    Edit zsh and tmux configs
    install baz, add keybinds for fzf, zoxide, tmux session toggling

--- platforms/arch/modules/shell-tools/setup.ts
+++ platforms/arch/modules/shell-tools/setup.ts
@@ -2,5 +2,5 @@ import { pacman } from "#arch/arch-util.ts"
 
 export default async function setup() {
   // ripgrep installed by setup
-  await pacman("tree fd coreutils jq zoxide")
+  await pacman("tree fd coreutils jq zoxide bat")
 }
