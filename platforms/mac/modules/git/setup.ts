55065274edb07bea6ef47b1aad3c22e76db3ed7b
Niels Nielsen <niels.nielsen@gmail.com>
2025-06-06 16:07:01 -0600

    make sure we call the shared setup to create our config file

--- platforms/mac/modules/git/setup.ts
+++ platforms/mac/modules/git/setup.ts
@@ -1,5 +1,7 @@
 import { brewBundle } from "#mac/mac-util.ts"
+import sharedGitSetup from "#shared/modules/git/setup.ts"
 
 export default async function setup() {
   await brewBundle(import.meta.dirname)
+  await sharedGitSetup()
 }
