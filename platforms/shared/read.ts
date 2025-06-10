733e1a704bed387f8ac26dfb0b9b4c62cfc007bb
Niels Nielsen <niels.nielsen@gmail.com>
2025-06-10 15:48:05 -0600

    clean up code

--- platforms/shared/read.ts
+++ platforms/shared/read.ts
@@ -16,30 +16,30 @@ export type ReadParams = {
 
 export const read = async ({ prompt = "Enter input: ", silent = false, replace = "" }: ReadParams) => {
   if (replace.length > 1) {
-    throw new Error("Replace must be empty string or a single character.")
+    throw new Error("Parameter 'replace' must be empty string or a single character.")
   }
 
-  const fd = await fs.open("/dev/tty", "r+")
+  const currentRawMode = process.stdin.isRaw
+  // get raw key presses immediately without any processing
+  // we'll handle the ctrl-c, delete, and enter key behavior ourselves
+  process.stdin.setRawMode(true)
+
+  const fd = await fs.open("/dev/tty", "r+") // open the tty file with read/write access
 
   await fd.write(prompt)
 
-  const currentRawMode = process.stdin.isRaw
-  if (silent) {
-    process.stdin.setRawMode(true)
-  }
-
   let line = ""
   let lineComplete = false
-
   const charBuffer = Buffer.alloc(1)
 
   while (!lineComplete) {
-    const data = await fd.read({ buffer: charBuffer, length: 1 })
+    const data = await fd.read({ buffer: charBuffer, length: 1 }) // read input one byte at a time
     const charCode = data.buffer[0]
     const char = String.fromCharCode(charCode)
 
+    // all the chars we are trying to match are 1 byte in utf-8
     switch (charCode) {
-      case keys.ctrlC: // Ctrl-c
+      case keys.ctrlC: // Ctrl-c - cancel input
         line = ""
         lineComplete = true
         break
@@ -47,12 +47,12 @@ export const read = async ({ prompt = "Enter input: ", silent = false, replace =
       case keys.backspace: // Backspace
       case keys.delete:
         if (line.length > 0) {
-          line = line.slice(0, -1)
+          line = line.slice(0, -1) // remove last char from line
           await fd.write("\r") // move to start of line
           await fd.write(" ".repeat(prompt.length + line.length + 1)) // clear line
           await fd.write("\r") // move back to start again
-          const outputText = silent ? replace.repeat(line.length) : line
-          await fd.write(prompt + outputText) // output passwordLength number of *'s
+          const outputText = silent ? replace.repeat(line.length) : line // replace chars with *'s if silent
+          await fd.write(prompt + outputText) // rewrite prompt and line with last char deleted
         }
         break
 
@@ -67,9 +67,7 @@ export const read = async ({ prompt = "Enter input: ", silent = false, replace =
     }
   }
 
-  if (silent) {
-    process.stdin.setRawMode(currentRawMode)
-  }
+  process.stdin.setRawMode(currentRawMode)
 
   return line
 }
