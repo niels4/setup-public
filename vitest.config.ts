91c676bb4e63088349637f7eb99a997b4290ee7d
Niels Nielsen <niels.nielsen@gmail.com>
2025-06-22 23:10:51 -0600

    add vitest and test scripts

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vitest/config"
+
+export default defineConfig({
+  test: {
+    silent: false,
+    coverage: {
+      reporter: ["text", "json", "html"],
+    },
+  },
+})
