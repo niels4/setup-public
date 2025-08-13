c977ce5c906b1879ceb775e57800a9c70905fdda
Niels Nielsen <niels.nielsen@gmail.com>
2025-08-13 11:20:34 -0600

    add ambr find replace example for reference

--- platforms/shared/modules/shell/scripts/find-replace.sh
+++ platforms/shared/modules/shell/scripts/find-replace.sh
@@ -5,3 +5,5 @@ rg "icons\/" src
 perl -p -i -e 's/icons\//assets\/icons\//g' `fd . src`
 
 perl -p -i -e 's|src/|live/|g' `fd . src`
+
+ambr --regex 'oldFunctionName\((.*), (.*)\)' 'newFunctionName($2, $1)' src
