3e0578c78654bb716fbb70dc25cd95edd3a9e113
Niels Nielsen <niels.nielsen@gmail.com>
2025-08-12 19:26:28 -0600

    don't wait for update to complete

--- platforms/arch/modules/docs/setup.ts
+++ platforms/arch/modules/docs/setup.ts
@@ -3,5 +3,6 @@ import { shell } from "#shared/src/util.ts"
 
 export default async function setup() {
   await pacman("man wikiman arch-wiki-docs tealdeer ")
-  await shell("tldr --update")
+  // no need to await this promise, this can update in the background while we move on
+  shell("tldr --update", { silent: true })
 }
