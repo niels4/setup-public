b3c8ee71d8247883bffeb9370f4044af35193e08
Niels Nielsen <niels.nielsen@gmail.com>
2026-01-21 01:33:40 -0700

    await the aur install

--- platforms/arch/modules/dev-misc/setup.ts
+++ platforms/arch/modules/dev-misc/setup.ts
@@ -2,6 +2,6 @@ import { aur } from "#arch/arch-util.ts"
 import sharedDevMiscSetup from "#shared/modules/dev-misc/setup.ts"
 
 export default async function setup() {
-  aur("shellcheck-bin ltex-ls-plus-bin")
+  await aur("shellcheck-bin ltex-ls-plus-bin")
   await sharedDevMiscSetup()
 }
