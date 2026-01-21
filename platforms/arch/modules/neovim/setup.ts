fd18cacd909851eb7594fdbcad7fd86ff6535b0a
Niels Nielsen <niels.nielsen@gmail.com>
2026-01-20 19:24:41 -0700

    cleanup neovim install

--- platforms/arch/modules/neovim/setup.ts
+++ platforms/arch/modules/neovim/setup.ts
@@ -1,9 +1,11 @@
 import { pacman } from "#arch/arch-util.ts"
 import sharedNeovimSetup from "#shared/modules/neovim/setup.ts"
 
+const packages = ["neovim", "unzip", "tree-sitter-cli"]
+
 export default async function setup() {
   // ripgrep installed by prereqs
   // fd installed by shell-tools
-  await pacman("neovim tree-sitter-cli stylua unzip")
+  await pacman(packages.join(" "))
   await sharedNeovimSetup()
 }
