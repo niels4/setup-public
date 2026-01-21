dda85f39152fca4bd672d96701efaee01bad3242
Niels Nielsen <niels.nielsen@gmail.com>
2026-01-20 19:21:07 -0700

    install git-lfs by default

--- platforms/arch/modules/git/setup.ts
+++ platforms/arch/modules/git/setup.ts
@@ -2,7 +2,7 @@ import { pacman } from "#arch/arch-util.ts"
 import sharedGitSetup from "#shared/modules/git/setup.ts"
 
 export default async function setup() {
-  // git already installed in setup.sh
-  await pacman("lazygit")
+  // git already installed in platforms/arch/setup.sh
+  await pacman("git-lfs lazygit")
   await sharedGitSetup()
 }
