dacc044834a5c6728a563a3df423042fa74ebe40
Niels Nielsen <niels.nielsen@gmail.com>
2026-01-20 19:25:54 -0700

    biome organize imports

--- platforms/mac/mac-util.ts
+++ platforms/mac/mac-util.ts
@@ -1,5 +1,5 @@
-import { shell } from "#shared/src/util.ts"
 import { join } from "node:path"
+import { shell } from "#shared/src/util.ts"
 
 export const brewBundle = async (dir: string) => {
   const brewfile = join(dir, "Brewfile")
