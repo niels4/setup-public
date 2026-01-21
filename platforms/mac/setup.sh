73e442a9a4f05f5f9699e225a749b37bf0b25aba
Niels Nielsen <niels.nielsen@gmail.com>
2026-01-20 19:24:19 -0700

    Users should now manually install homebrew first (automatically installing homebrew was sometimes flakey, this just simplifies things and doesn't require the user to enter their root password)

--- platforms/mac/setup.sh
+++ platforms/mac/setup.sh
@@ -1,46 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 setup_dir=$(dirname "$(realpath "$0")")
 
-
-if [ ! -d /opt/homebrew ];then
-
-  echo "Enter user password to install homebrew:"
-  IFS= read -rs USER_PW
-  sudo -k # clear sudo credentials
-  if echo "$USER_PW" | sudo -S -v &> /dev/null; then
-    echo "\nPassword confirmed."
-  else
-    echo "\nInvalid password."
-    sudo -k
-    exit 1
-  fi
-  sudo -k # clear sudo credentials
-
-  homebrew_install_script="/tmp/homebrew_install.sh"
-  echo '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"' > ${homebrew_install_script}
-  chmod +x ${homebrew_install_script}
-
-  expect << EOF
-set timeout -1
-spawn ${homebrew_install_script}
-
-expect "Password:"
-send -- "$USER_PW\r"
-
-expect -re {to continue or any other key to abort:\r\n$}
-send -- "\r"
-
-expect eof
-EOF
-
-  USER_PW=""
-
-  echo "Homebrew installed, run brew doctor to verify"
+if ! command -v brew >/dev/null 2>&1;then
+  echo "'brew' command not found. Visit 'https://brew.sh' to follow installation instructions (copy and paste script)."
+  open "https://brew.sh"
+  exit 1
 fi
-
-# add homebrew to the path
-eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # install latest version of nodejs
 brew install fnm
