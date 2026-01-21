4dc90a92f251f348b3f1df7961bdb3fd94c3a6c8
Niels Nielsen <niels.nielsen@gmail.com>
2026-01-20 21:03:17 -0700

    move homebrew shell setup from .zshenv to .zshrc

--- platforms/mac/modules/prereqs/setup.ts
+++ platforms/mac/modules/prereqs/setup.ts
@@ -5,14 +5,12 @@ import { ensureFile } from "#shared/src/fs.ts"
 import { addLineToZshenv, shell, shellIsSuccessful } from "#shared/src/util.ts"
 
 const homebrewUpdateRate = `export HOMEBREW_AUTO_UPDATE_SECS=43200`
-const homebrewEnv = `eval "$(/opt/homebrew/bin/brew shellenv)"`
 
 // Add required utilities and environment variables that all scripts can rely on
 export default async function setup() {
+  await brewBundle(import.meta.dirname)
   await ensureFile(zshenv)
   await addLineToZshenv(homebrewUpdateRate)
-  await addLineToZshenv(homebrewEnv)
-  await brewBundle(import.meta.dirname)
   if (!(await shellIsSuccessful("which cargo"))) {
     await shell("rustup-init -y")
   }
--- platforms/mac/modules/shell/setup.ts
+++ platforms/mac/modules/shell/setup.ts
@@ -1,7 +1,18 @@
+import { join } from "node:path"
 import { brewBundle } from "#mac/mac-util.ts"
 import sharedShellSetup from "#shared/modules/shell/setup.ts"
+import { zshAutorunDir } from "#shared/src/constants.ts"
+import { ensureSymlink } from "#shared/src/fs.ts"
+
+const __dirname = import.meta.dirname
+
+const homebrewZshConfigLink = {
+  src: join(__dirname, "zshrc.d", "homebrew.sh"),
+  dst: join(zshAutorunDir, "homebrew.sh"),
+}
 
 export default async function setup() {
   await brewBundle(import.meta.dirname)
-  await sharedShellSetup()
+  await sharedShellSetup() // this creates the zshAutorunDir
+  await ensureSymlink(homebrewZshConfigLink) // add mac specific call to setup homebrew path
 }
