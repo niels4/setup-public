53c3b598b873157ae6c0478b546334211ba97906
Niels Nielsen <niels@Nielss-MacBook-Pro-2.local>
2026-01-30 22:06:15 -0700

    install and setup direnv

--- platforms/shared/modules/dev-misc/setup.ts
+++ platforms/shared/modules/dev-misc/setup.ts
@@ -1,6 +1,17 @@
+import { join } from "node:path"
+import { zshAutorunDir } from "#shared/src/constants.ts"
+import { ensureSymlink } from "#shared/src/fs.ts"
 import { shell } from "#shared/src/util.ts"
+
+const __dirname = import.meta.dirname
+
+const devMiscZshConfigLink = {
+  src: join(__dirname, "zshrc.d", "dev-misc.sh"),
+  dst: join(zshAutorunDir, "dev-misc.sh"),
+}
 
 export default async function setup() {
   await shell("npm install -g bash-language-server yaml-language-server")
   await shell("cargo install taplo-cli")
+  await ensureSymlink(devMiscZshConfigLink) // setup direnv
 }
