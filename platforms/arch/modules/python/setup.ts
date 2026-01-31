cffdca2733ba8596759fe352f6093b668acbe66d
Niels Nielsen <niels@Nielss-MacBook-Pro-2.local>
2026-01-30 22:06:47 -0700

    install pipx

--- platforms/arch/modules/python/setup.ts
+++ platforms/arch/modules/python/setup.ts
@@ -1,6 +1,6 @@
 import { pacman } from "#arch/arch-util.ts"
 
-const packages = ["python", "python-isort", "python-black", "ruff"]
+const packages = ["python", "python-isort", "python-black", "ruff", "python-pipx"]
 
 export default async function setup() {
   await pacman(packages.join(" "))
